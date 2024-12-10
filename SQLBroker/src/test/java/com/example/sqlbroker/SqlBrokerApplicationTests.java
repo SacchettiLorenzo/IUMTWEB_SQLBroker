ac8f9b0802d8452f569e0fb21bbab7e3fc7efa02
Sacchetti Lorenzo <sacchettibusiness@gmail.com>
2024-12-10 21:57:25 +0100

    sql borker in repo

---
+++ SQLBroker/src/test/java/com/example/sqlbroker/SqlBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sqlbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlBrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
