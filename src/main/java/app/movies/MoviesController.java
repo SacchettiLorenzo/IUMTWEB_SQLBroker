c07da9f7937c8a7e6c629a1d3f06c486256d7a0d
Sacchetti Lorenzo <sacchettibusiness@gmail.com>
2024-12-27 02:06:31 +0100

    generic pagination and sorting for Movies

--- src/main/java/app/movies/MoviesController.java
+++ src/main/java/app/movies/MoviesController.java
@@ -1,11 +1,16 @@
 package app.movies;
 
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 @RestController
@@ -14,14 +19,32 @@ public class MoviesController {
 
     MoviesService moviesService;
 
+    List<String> classFields = new ArrayList<>();
 
     public MoviesController(MoviesService moviesService) {
         this.moviesService = moviesService;
+        for (Field field : Movies.class.getDeclaredFields()) {
+            classFields.add(field.getName().toLowerCase());
+        }
     }
 
     @GetMapping
-    public Page<Movies> findAll (@RequestParam int page, @RequestParam int size){
+    public Page<Movies> findAll (@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size, @RequestParam(required = false, defaultValue = "Id") String sortParam, @RequestParam(required = false, defaultValue = "ASC") String sortDirection) {
         PageRequest pageRequest = PageRequest.of(page, size);
+        if(sortParam != null) {
+            Sort.Direction tempSortDirection = Sort.Direction.ASC;
+            String tempSortParam = "Id";
+            if(sortDirection != null) {
+                if(sortDirection.equalsIgnoreCase("DESC") || sortDirection.equalsIgnoreCase("ASC")) {
+                    tempSortDirection = sortDirection.equalsIgnoreCase("DESC") ? Sort.Direction.DESC : Sort.Direction.ASC;
+                }
+            }
+
+            if(classFields.contains(sortParam.toLowerCase())) {
+                tempSortParam = sortParam.toLowerCase();
+            }
+            pageRequest = pageRequest.withSort(Sort.by(tempSortDirection, tempSortParam));
+        }
         return moviesService.findAll(pageRequest);
     }
 
