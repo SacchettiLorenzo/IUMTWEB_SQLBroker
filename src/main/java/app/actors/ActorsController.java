315ab5f0e3e278bdee16b21c736edb0d56fc02ec
Sacchetti Lorenzo <sacchettibusiness@gmail.com>
2024-12-27 11:09:14 +0100

    generic pagination and sorting for actors and crew

--- src/main/java/app/actors/ActorsController.java
+++ src/main/java/app/actors/ActorsController.java
@@ -5,13 +5,16 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.management.ObjectName;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -21,13 +24,32 @@ public class ActorsController {
 
     ActorsService actorsService;
 
+    List<String> classFields = new ArrayList<>();
+
     public ActorsController(ActorsService actorsService) {
         this.actorsService = actorsService;
+        for (Field field : Actors.class.getDeclaredFields()) {
+            classFields.add(field.getName().toLowerCase());
+        }
     }
 
     @GetMapping
-    public Page<Actors> findAll(@RequestParam int page, @RequestParam int size){
+    public Page<Actors> findAll(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size, @RequestParam(required = false, defaultValue = "Id") String sortParam, @RequestParam(required = false, defaultValue = "ASC") String sortDirection){
         PageRequest pageRequest = PageRequest.of(page, size);
+        if(sortParam != null) {
+            Sort.Direction tempSortDirection = Sort.Direction.ASC;
+            String tempSortParam = "Id";
+            if(sortDirection != null) {
+                if(sortDirection.equalsIgnoreCase("DESC") || sortDirection.equalsIgnoreCase("ASC")) {
+                    tempSortDirection = sortDirection.equalsIgnoreCase("DESC") ? Sort.Direction.DESC : Sort.Direction.ASC;
+                }
+            }
+
+            if(classFields.contains(sortParam.toLowerCase())) {
+                tempSortParam = sortParam.toLowerCase();
+            }
+            pageRequest = pageRequest.withSort(Sort.by(tempSortDirection, tempSortParam));
+        }
         return actorsService.findAll(pageRequest);
     }
 
--- src/main/java/app/crew/CrewController.java
+++ src/main/java/app/crew/CrewController.java
@@ -4,12 +4,15 @@ import app.actors.Actors;
 import app.movies.Movies;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -17,13 +20,32 @@ import java.util.Map;
 public class CrewController {
     CrewService crewService;
 
+    List<String> classFields = new ArrayList<>();
+
     public CrewController(CrewService crewService) {
         this.crewService = crewService;
+        for (Field field : Crew.class.getDeclaredFields()) {
+            classFields.add(field.getName().toLowerCase());
+        }
     }
 
     @GetMapping
-    public Page<Crew> findAll (@RequestParam int page, @RequestParam int size){
+    public Page<Crew> findAll (@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size, @RequestParam(required = false, defaultValue = "Id") String sortParam, @RequestParam(required = false, defaultValue = "ASC") String sortDirection){
         PageRequest pageRequest = PageRequest.of(page, size);
+        if(sortParam != null) {
+            Sort.Direction tempSortDirection = Sort.Direction.ASC;
+            String tempSortParam = "Id";
+            if(sortDirection != null) {
+                if(sortDirection.equalsIgnoreCase("DESC") || sortDirection.equalsIgnoreCase("ASC")) {
+                    tempSortDirection = sortDirection.equalsIgnoreCase("DESC") ? Sort.Direction.DESC : Sort.Direction.ASC;
+                }
+            }
+
+            if(classFields.contains(sortParam.toLowerCase())) {
+                tempSortParam = sortParam.toLowerCase();
+            }
+            pageRequest = pageRequest.withSort(Sort.by(tempSortDirection, tempSortParam));
+        }
         return crewService.findAll(pageRequest);
     }
 
